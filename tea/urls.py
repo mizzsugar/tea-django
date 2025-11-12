8b5ce5dbe420c2106f7b5b6567dd8518a0ada71f
mizuki sato <mizki.sato.0025@gmail.com>
2025-11-12 18:34:28 +0900

    create published tea detail page

--- tea/urls.py
+++ tea/urls.py
@@ -4,4 +4,5 @@ from tea import views
 
 urlpatterns = [
     path("", views.published_tea_list, name="published_tea_list"),
+    path("teas/<int:tea_id>/", views.published_tea_detail, name="published_tea_detail"),
 ]
--- tea/views.py
+++ tea/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from tea.models import Tea
 
@@ -7,3 +7,9 @@ def published_tea_list(request):
     now = timezone.now()
     teas = Tea.objects.filter(published_at__isnull=False, published_at__lt=now)
     return render(request, 'tea/published_tea_list.html', {'teas': teas})
+
+
+def published_tea_detail(request, tea_id: int):
+    now = timezone.now()
+    tea = get_object_or_404(Tea, id=tea_id, published_at__isnull=False, published_at__lt=now)
+    return render(request, 'tea/published_tea_detail.html', {'tea': tea})
